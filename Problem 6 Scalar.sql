c086294d8a86bfe29f8bac266dd6af669bf3ce2f
jiagalvezabella <120723124+jiagalvezabella@users.noreply.github.com>
2024-09-23 03:51:57 +0800

    Create Problem 6 Scalar.sql

---
+++ Problem 6 Scalar.sql
@@ -0,0 +1,5 @@
+SELECT	p.projno, COALESCE(p.majproj, 'MAIN PROJECT') AS majproj, 
+		CONCAT(e.firstname , ' ', e.lastname, ' ', DATENAME(WEEKDAY, p.prstdate), ' ',p.prstdate) AS 'EMP-NAME'
+FROM	project p JOIN employee e on p.respemp = e.empno
+WHERE	p.projno LIKE 'MA%'
+ORDER BY 1;
